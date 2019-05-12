4c4083292810fa74f9f2b0bffc38b1cc602c29e9
Du Yuyang <du.yuyang@rea-group.com>
2019-05-12 19:34:42 +1000

    lambda take s3 event

--- queue-stack/queue-up/main.go
+++ queue-stack/queue-up/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"context"
-	"fmt"
+	"log"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -11,7 +11,7 @@ import (
 func handler(ctx context.Context, s3Event events.S3Event) {
 	for _, record := range s3Event.Records {
 		s3 := record.S3
-		fmt.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
+		log.Printf("[%s - %s] Bucket = %s, Key = %s \n", record.EventSource, record.EventTime, s3.Bucket.Name, s3.Object.Key)
 	}
 }
 
